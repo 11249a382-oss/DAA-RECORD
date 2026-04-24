838c2165975e7ca231773e57e15f39ab32d8d047
11249a382-oss <11249a382@kanchiuniv.ac.in>
2026-04-24 13:22:30 +0530

    Clean up output comments in Ford-Fulkerson implementation
    Removed example output and result comments from the code.

--- Network Flow (Ford–Fulkerson).c
+++ Network Flow (Ford–Fulkerson).c
@@ -41,16 +41,7 @@ int main()
     return 0;
 }
 
-Output:
-Enter number of vertices: 4
-Enter capacity matrix:
-0 10 10 0
-0 0 5 10
-0 0 0 10
-0 0 0 0
-Enter source and sink: 0 3
 
-Maximum flow: 20
   
 Result:
 
